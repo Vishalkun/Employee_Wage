1a0fab733b57d35562417044e9c4c541cb4c268c
Vishal github <Vishal.kunchal@gmail.com>
2023-08-28 19:36:34 +0530

    [vishal kunchal]:Emp present or not

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿namespace PractisePrograms
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int IS_FULL_TIME = 1;
+            Random R=new Random();  
+            int empcheck= new Random().Next(0,2);
+            if(empcheck== IS_FULL_TIME)
+            {
+                Console.WriteLine(" Employee is Present");
+            }
+            else
+            {
+                Console.WriteLine("Employe is not present");
+            }
+        }
+    }
+}
