ade6841f09a8520fa37d0265e52440b98967fa03
Vishal github <Vishal.kunchal@gmail.com>
2023-08-29 18:59:03 +0530

    [vishal kunchal]: Calcuating wages for month & working days usecase5

--- Program.cs
+++ Program.cs
@@ -8,26 +8,35 @@
           const  int IS_PART_TIME = 1;
             const int IS_FULL_TIME = 2;
            const int Emp_Rate_Per_Hour = 20;
+            const int NO_OF_WORKING_DAYS = 2;
 
             int empHrs = 0;
             int empWage = 0;
-            Random R=new Random();  
-            int empcheck= new Random().Next(0,3);
-            
-            switch(empcheck)
+            int totalEmpWage =0;
+            for (int day = 1; day < NO_OF_WORKING_DAYS; day++)
             {
-                case IS_PART_TIME :
-                    empHrs=4;
-                    break;
-                case IS_FULL_TIME :
-                    empWage=8;
-                    break;
-                default:
-                    empHrs = 0;
-                    break;
+                Random R = new Random();
+                int empcheck = new Random().Next(0, 3);
+
+                switch (empcheck)
+                {
+                    case IS_PART_TIME:
+                        empHrs = 4;
+                        break;
+                    case IS_FULL_TIME:
+                        empWage = 8;
+                        break;
+                    default:
+                        empHrs = 0;
+                        break;
+                }
+                empWage = empHrs * Emp_Rate_Per_Hour;
+                totalEmpWage += empWage;
+                Console.WriteLine("Emp Wage :" + empWage);
             }
-            empWage = empHrs * Emp_Rate_Per_Hour;
-            Console.WriteLine("Emp Wage :" + empWage);
+            Console.WriteLine(" TotalEmpWage :" + totalEmpWage);
+
         }
+
     }
 }
