ea90f8a5e73ec54dc3af610e6370d6e26bef1b30
Vishal github <Vishal.kunchal@gmail.com>
2023-08-29 19:34:28 +0530

    [vishal kunchal]: Compute employee  in compamy usecase8

--- Program.cs
+++ Program.cs
@@ -4,18 +4,15 @@
     {
         const int IS_PART_TIME = 1;
         const int IS_FULL_TIME = 2;
-        const int Emp_Rate_Per_Hour = 20;
-        const int NO_OF_WORKING_DAYS = 2;
-        const int MAX_HOURS_IN_MONTH = 10;
-
-        public static int computeEmpWage()
+        
+        public static int computeEmpWage(string company,int empRatePerHour,int numOfWorkingDays,int maxHoursPerMonth)
         {
             
          
             int empHrs = 0;
             int totalEmpHours = 0;
             int totalWorkingDays =0;
-            while (totalEmpHours<= MAX_HOURS_IN_MONTH  && totalWorkingDays< NO_OF_WORKING_DAYS)
+            while (totalEmpHours<= maxHoursPerMonth && totalWorkingDays< numOfWorkingDays)
             {
                 totalWorkingDays++;
                 Random R = new Random();
@@ -36,13 +33,14 @@
                 totalEmpHours += empHrs;
                 Console.WriteLine("Day#:" + totalWorkingDays + " Emp Hrs :" + empHrs);
             }
-            int totalEmpWage = totalEmpHours * Emp_Rate_Per_Hour;
-                Console.WriteLine(" Total Emp Wage :" + totalEmpWage);
+            int totalEmpWage = totalEmpHours * empRatePerHour;
+                Console.WriteLine(" Total Emp Wage for company:"+company +"is:"+ totalEmpWage);
             return totalEmpWage;
         }
         static void Main(string[] args)
         {
-            computeEmpWage();   
+            computeEmpWage("DMart",20,2,10);
+            computeEmpWage("Reliance", 10, 4, 20);
 
         }
 
