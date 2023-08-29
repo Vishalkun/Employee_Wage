d88b9e916ff407f5de9e7dd4eb45aa3a90666298
Vishal github <Vishal.kunchal@gmail.com>
2023-08-29 18:30:26 +0530

    [vishal kunchal]: Adding part time employee & wage usecase3

--- Program.cs
+++ Program.cs
@@ -4,14 +4,20 @@
     {
         static void Main(string[] args)
         {
-            int IS_FULL_TIME = 1;
+            
+            int IS_PART_TIME = 1;
+            int IS_FULL_TIME = 2;
             int Emp_Rate_Per_Hour = 20;
 
             int empHrs = 0;
             int empWage = 0;
             Random R=new Random();  
             int empcheck= new Random().Next(0,2);
-            if(empcheck== IS_FULL_TIME)
+            if(empcheck== IS_PART_TIME)
+            {
+                empHrs = 4;
+            }
+            else if(empcheck== IS_FULL_TIME)
             {
                 empHrs = 8;
             }
