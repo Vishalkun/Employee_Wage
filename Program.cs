8718f535d67f75fd1c11b903f7ce675f20478aac
Vishalkun <91224087+Vishalkun@users.noreply.github.com>
2023-08-29 19:44:11 +0530

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -24,7 +24,7 @@
                         empHrs = 4;
                         break;
                     case IS_FULL_TIME:
-                        empWage = 8;
+                        empHrs = 8;
                         break;
                     default:
                         empHrs = 0;
